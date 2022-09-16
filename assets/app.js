000c3e8bdfd0c8708722a2b54867c9ac76f95c02
christian <arthurchristian111@gmail.com>
2022-09-16 12:58:50 +0000

    added eventListener to the next button

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,15 @@
-const image = document.getElementById("image");
+const img = document.getElementById("image");
 const job = document.getElementById("job");
 const author = document.getElementById("author");
 const text = document.getElementById("text");
-const previousButton = document.getElementById("previous-button");
-const nextButton = document.getElementById("next-button");
+const previousButton = document.getElementById("previousButton");
+const nextButton = document.getElementById("nextButton");
 
-const reviews = [{
+const reviews = [person = {
         id: 1,
         name: 'Jonas',
         job: 'full stack web developer',
-        image: '<img src="./70929863_2560581960831519_5638313021852876800_n.jpg" class="rounded-circle" id="image" style="width: 150px;" alt="Avatar"/>',
+        img: '<img src="./70929863_2560581960831519_5638313021852876800_n.jpg" class="rounded-circle" id="image" style="width: 150px;" alt="Avatar"/>',
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua."
     },
 
@@ -17,34 +17,46 @@ const reviews = [{
         id: 2,
         name: 'Micheal',
         job: 'full stack web developer',
-        image: '<img src="https://mdbcdn.b-cdn.net/img/new/avatars/5.webp" class="rounded-3" style="width: 150px;" alt="Avatar" />',
-        text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        img: '<img src="https://mdbcdn.b-cdn.net/img/new/avatars/5.webp" class="rounded-3" style="width: 150px;" alt="Avatar" />',
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua."
     },
 
     {
         id: 3,
         name: 'Christian',
         job: 'full stack web developer',
-        image: 'img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" style="width: 150px;" alt="Avatar"/>',
-        text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        img: 'img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" style="width: 150px;" alt="Avatar"/>',
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua."
     },
 
     {
         id: 4,
         name: 'Philip',
         job: 'full stack web developer',
-        image: 'img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" style="width: 150px;" alt="Avatar"/>',
-        text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        img: 'https://mdbcdn.b-cdn.net/img/new/avatars/2.webp',
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
     },
 ];
 
 let currentItem = 0;
 
 window.addEventListener("DOMContentLoaded", function () {
-    const item = reviews[currentItem];
-    image.src = item.image;
-    author.innerHTML = item.name;
-    job.innerHTML = item.job;
-    text.innerHTML = item.text
+    showPerson(currentItem)
 
+})
+
+const showPerson = (person) => {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    text.textContent = item.text
+}
+
+nextButton.addEventListener("click", function () {
+    currentItem++
+    if (currentItem > reviews.length - 1) {
+        return currentItem = 0;
+    }
+    showPerson(currentItem)
 })
